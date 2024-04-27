4e953d9de0f3cb70398b2f4f364ce0f286ddbf04
suja <mahrjansuja222@gmail.com>
2024-04-27 21:30:10 +0545

    set up mongodb connection in backend

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const mongoURL="mongodb://localhost:27017/";
+const connectToMongo = async () => {
+    try {
+      mongoose.set("strictQuery", false);
+      mongoose.connect(mongoURL);
+      console.log("Connected to Mongo Successfully!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+module.exports=connectToMongo;
